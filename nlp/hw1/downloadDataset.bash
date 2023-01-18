b22615eb2fd05d41677bf770e47e8768e28cad6e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-18 16:47:10 -0600

    Add data

---
+++ nlp/hw1/downloadDataset.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O positiveSentiment.txt https://raw.githubusercontent.com/dennybritz/cnn-text-classification-tf/master/data/rt-polaritydata/rt-polarity.pos
+wget -O negativeSentiment.txt https://raw.githubusercontent.com/dennybritz/cnn-text-classification-tf/master/data/rt-polaritydata/rt-polarity.neg
