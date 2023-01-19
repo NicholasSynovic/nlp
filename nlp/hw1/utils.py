06fc179105d2d2068cd25868c403db5cc4bb2825
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-18 18:21:06 -0600

    Handle data splits

--- nlp/hw1/utils.py
+++ nlp/hw1/utils.py
@@ -1,10 +1,15 @@
 from pathlib import PurePath
+from numpy.random import RandomState
+from numpy import ndarray
 
-def loadDataTXT(filepath: PurePath)    ->  list:
+def loadDataTXT(filepath: PurePath, keepSplit: float = 0.6, seed: int = 42)    ->  ndarray:
+    random: RandomState = RandomState(seed)
+
     with open(filepath, "r") as dataFile:
         data: list = dataFile.readlines()
         dataFile.close()
 
     data = [d.strip() for d in data]
+    data = random.choice(data, size=round(len(data) * keepSplit), replace=False)
 
     return data
