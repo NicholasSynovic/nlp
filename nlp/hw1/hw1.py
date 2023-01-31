a385d615fadf0b7a144977c4f5b7f5ee9e2cc469
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-30 22:14:05 -0600

    Start work on the hw

--- nlp/hw1/hw1.py
+++ nlp/hw1/hw1.py
@@ -1,17 +1,53 @@
+from requests import get, Response
 from pathlib import PurePath
+from typing import List, Tuple
+from numpy.random import RandomState
+from numpy import ndarray
+from math import floor
 
-import tokenizer
-from utils import loadDataTXT
+def downloadData(url: str, filepath: PurePath)  -> None:
+    resp: Response = get(url)
+
+    text: str = resp.content.decode(encoding="UTF-8")
+    lines: List[str] = text.split(sep="\n")
+    lines = [f"{line}\n" for line in lines]
+
+    with open(file=filepath, mode="w") as sentiment:
+        sentiment.writelines(lines)
+        sentiment.close()
+
+def tokenize(filepath: PurePath)    ->  Tuple[List[str], List[str]]:
+    tokens: List[str] = []
+    with open(file=filepath, mode="r") as sentiment:
+        lines: List[str] = sentiment.readlines()
+        sentiment.close()
+
+    line: str
+    for line in lines:
+        foo: List[str] = line.split(sep=" ")
+        tokens.extend(foo)
+
+    tokens: set[str] = set(tokens)
+    tokens: List[str] = list(tokens)
+
+    return tokens
+
+def main()  ->  None:
+    tokens: dict = {}
+    positiveSentiment: PurePath = PurePath("positive")
+    negativeSentiment: PurePath = PurePath("negative")
+
+    downloadData(url="https://raw.githubusercontent.com/dennybritz/cnn-text-classification-tf/master/data/rt-polaritydata/rt-polarity.pos", filepath=positiveSentiment)
+    downloadData(url="https://raw.githubusercontent.com/dennybritz/cnn-text-classification-tf/master/data/rt-polaritydata/rt-polarity.neg", filepath=negativeSentiment)
+
+    positiveTokens: List[str] = splitData(filepath=positiveSentiment)
+    # negativeTokens: List[str] = tokenize(filepath=negativeSentiment)
+
+    # tokens["positive"] = positiveTokens
+    # tokens["negative"] = negativeTokens
 
 
-def main() -> None:
-    positiveDataFile: PurePath = PurePath("positiveSentiment.txt")
-    negativeDataFile: PurePath = PurePath("negativeSentiment.txt")
-
-    positiveDataFile: list = loadDataTXT(positiveDataFile)
-    negativeDataFile: list = loadDataTXT(negativeDataFile)
 
 
 if __name__ == "__main__":
-    tokenizer.main()
     main()
