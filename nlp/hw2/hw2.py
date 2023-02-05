040adc949903bceed62a2a131f0f1c11c9aab41c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-02-05 13:37:51 -0600

    Write code to load data and remove stop words

--- nlp/hw2/hw2.py
+++ nlp/hw2/hw2.py
@@ -16,6 +16,36 @@ def downloadData(url: str, filepath: PurePath) -> None:
         sentiment.close()
 
 
+def loadData(filepath: PurePath, stopwords: PurePath) -> set[str]:
+    data: List[str]
+    stopwordsData: List[str]
+
+    with open(filepath, "r") as dataFile:
+        data = dataFile.readlines()
+        dataFile.close()
+
+    with open(stopwords, "r") as stopwordsFile:
+        stopwordsData = stopwordsFile.readlines()
+        stopwordsFile.close()
+
+    data = [d.strip() for d in data]
+    stopwordsData = [d.strip() for d in stopwordsData]
+
+    stopwordsSet: set[str] = set(stopwordsData)
+
+    idx: int
+    for idx in range(len(data)):
+        tokens: List[str] = set(
+            [token for token in data[idx].split(" ") if token.isalpha()]
+        )
+
+        tokens = tokens - stopwordsSet
+
+        data[idx] = " ".join(tokens)
+
+    return data
+
+
 def main() -> None:
     positiveSentiment: PurePath = PurePath("positive")
     negativeSentiment: PurePath = PurePath("negative")
@@ -29,6 +59,9 @@ def main() -> None:
         url="https://raw.githubusercontent.com/dennybritz/cnn-text-classification-tf/master/data/rt-polaritydata/rt-polarity.neg",
         filepath=negativeSentiment,
     )
+
+    positveData: List[str] = loadData(filepath=positiveSentiment, stopwords=stopwords)
+    negativeData: List[str] = loadData(filepath=negativeSentiment, stopwords=stopwords)
 
 
 if __name__ == "__main__":
