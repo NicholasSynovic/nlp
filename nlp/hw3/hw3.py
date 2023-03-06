fb35d07efde7d3db4d6cbb3514b18884f1b4a883
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-06 14:46:27 -0600

    Reimpliment data splitter

--- nlp/hw3/hw3.py
+++ nlp/hw3/hw3.py
@@ -1,5 +1,6 @@
+from collections import Counter
 from pathlib import PurePath
-from typing import List
+from typing import List, Tuple
 
 from sklearn.model_selection import train_test_split
 
@@ -25,24 +26,59 @@ def loadData(filepath: PurePath) -> List[str]:
     return data
 
 
-def main() -> None:
-    tempData: List[str]
-    testingData: List[str]
-    trainingData: List[str]
-    developmentData: List[str]
+def splitData(
+    positiveData: List[str], negativeData: List[str]
+) -> List[Tuple[List[str], List[str], List[str]]]:
+    positiveTempData = List[str]
+    negativeTempData = List[str]
 
+    positiveTempData, positiveTestingData = train_test_split(
+        positiveData, test_size=0.15, train_size=0.85, random_state=42, shuffle=True
+    )
+    positiveTrainingData, positiveDevelopmentData = train_test_split(
+        positiveTempData, test_size=0.15, train_size=0.7, random_state=42, shuffle=True
+    )
+    negativeTempData, negativeTestingData = train_test_split(
+        negativeData, test_size=0.15, train_size=0.85, random_state=42, shuffle=True
+    )
+    negativeTrainingData, negativeDevelopmentData = train_test_split(
+        negativeTempData, test_size=0.15, train_size=0.7, random_state=42, shuffle=True
+    )
+
+    return [
+        (positiveTrainingData, positiveDevelopmentData, positiveTestingData),
+        (negativeTrainingData, negativeDevelopmentData, negativeTestingData),
+    ]
+
+
+def termDocumentFrequency(data: List[str]) -> None:
+    wordList: List[str] = []
+
+    document: str
+    for document in data:
+        wordList += document.split(" ")
+
+    td: dict[str, List] = {document: [] for document in data}
+
+    wordSet: set[str] = set(wordList)
+
+    for document in td:
+        c: Counter = Counter(document.split(" "))
+        for word in wordSet:
+            td[document].append(c[word])
+
+        print(td[document])
+
+
+def main() -> None:
     positivePath: PurePath = PurePath("positive")
     negativePath: PurePath = PurePath("negative")
 
     positiveData: List[str] = loadData(filepath=positivePath)
     negativeData: List[str] = loadData(filepath=negativePath)
-    data: List[str] = positiveData + negativeData
 
-    tempData, testingData = train_test_split(
-        data, test_size=0.15, train_size=0.85, random_state=42, shuffle=True
-    )
-    trainingData, developmentdata = train_test_split(
-        tempData, test_size=0.15, train_size=0.70, random_state=42, shuffle=True
+    data: List[Tuple[List[str], List[str], List[str]]] = splitData(
+        positiveData, negativeData
     )
 
 
