642ef030a2b854d3a1378f921df89450bbea5823
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-23 09:58:39 -0500

    Create training code for word2vec model

--- nlp/hw4/downloadCorpus.bash
+++ nlp/hw4/downloadCorpus.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# wget -O corpus.zip https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip?ref=blog.salesforceairesearch.com
+wget -O corpus.zip https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip?ref=blog.salesforceairesearch.com
 
 unzip corpus.zip
--- nlp/hw4/hw4.py
+++ nlp/hw4/hw4.py
@@ -1,5 +1,6 @@
-import gensim.models
 from gensim import utils
+from gensim.models import Word2Vec
+from progress.bar import Bar
 
 
 class MyCorpus:
@@ -7,15 +8,38 @@ class MyCorpus:
     Code from https://radimrehurek.com/gensim/auto_examples/tutorials/run_word2vec.html#sphx-glr-auto-examples-tutorials-run-word2vec-py
     """
 
+    epochCount: int = 1
+
     def __iter__(self):
         corpus_path = "wikitext-103/wiki.train.tokens"
-        line: str
-        for line in open(corpus_path):
-            if (line.isspace()) or line[0:1] == "=":
-                continue
-            line: str = line.strip()
-            yield utils.simple_preprocess(line)
+        num_lines = sum(1 for line in open(corpus_path))
+
+        with Bar(f"Iterating through {corpus_path}...", max=num_lines) as bar:
+            line: str
+            for line in open(corpus_path):
+                if (line.isspace()) or line[0:1] == "=":
+                    bar.next()
+                    continue
+                line: str = line.strip()
+                yield utils.simple_preprocess(line)
+                bar.next()
+
+        self.epochCount += 1
 
 
-sentences = MyCorpus()
-model = gensim.models.Word2Vec(sentences=sentences)
+def train(modelFilePath: str = "model/w2v.gensim") -> None:
+    sentences: MyCorpus = MyCorpus()
+
+    print("Creating Word2Vec model...")
+    model: Word2Vec = Word2Vec(sentences=sentences)
+
+    print(f"Saving model to {modelFilePath}...")
+    model.save(modelFilePath)
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
