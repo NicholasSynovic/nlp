f68d864f456ac614d8a9580ec0db29ba5e0c38b5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-23 09:29:08 -0500

    Add code to train word2vec

---
+++ nlp/hw4/downloadCorpus.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# wget -O corpus.zip https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip?ref=blog.salesforceairesearch.com
+
+unzip corpus.zip
---
+++ nlp/hw4/hw4.py
@@ -0,0 +1,23 @@
+from gensim import utils
+from gensim.test.utils import datapath
+
+
+class MyCorpus:
+    """An iterator that yields sentences (lists of str).
+    Code from https://radimrehurek.com/gensim/auto_examples/tutorials/run_word2vec.html#sphx-glr-auto-examples-tutorials-run-word2vec-py
+    """
+
+    def __iter__(self):
+        corpus_path = "wikitext-103/wiki.train.tokens"
+        line: str
+        for line in open(corpus_path):
+            if (line.isspace()) or line[0:1] == "=":
+                continue
+            line: str = line.strip()
+            yield utils.simple_preprocess(line)
+
+
+import gensim.models
+
+sentences = MyCorpus()
+model = gensim.models.Word2Vec(sentences=sentences)
